114cc8343403657f5d99103c175cbd28b5f01ac0
walid horchani <walid.horchani@oyez.fr>
2020-09-23 08:53:48 +0100

    styling search component

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -5,18 +5,27 @@ import FeatherIcon from 'react-native-vector-icons/Feather';
 const SearchBox = (props) => {
   return (
     <View style={styles.searchContainer}>
-      <FeatherIcon name="search" size={24} />
-      <TextInput placeholder={props.placeholder} />
+      <View style={styles.iconContainer}>
+        <FeatherIcon name="search" size={25} />
+      </View>
+      <View>
+        <TextInput placeholder={props.placeholder} />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   searchContainer: {
+    flexDirection: 'row',
+    alignItems: 'center',
     width: '90%',
     height: 50,
     borderRadius: 5,
     backgroundColor: '#C0C0C0',
+  },
+  iconContainer: {
+    marginHorizontal: 15,
   },
 });
 
