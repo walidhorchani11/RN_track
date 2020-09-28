f44a852e2389cedf8af9593b9c02e337236275d3
Horchani Walid <walidhorchani11@gmail.com>
2020-09-28 21:17:51 +0100

    authenticate request

--- App.js
+++ App.js
@@ -1,19 +1,29 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import {View} from 'react-native';
-import {ApolloProvider} from '@apollo/client';
+import {ApolloProvider, HttpLink} from '@apollo/client';
 
 import client from './src/graphql/client';
+import AuthProvider, {AuthContext} from './src/context/AuthContext';
+
 import EquipmentScreen from './src/screens/EquipmentScreen';
 import AuthScreen from './src/screens/AuthScreen';
 
 const App = () => {
+  
+const authContext = useContext(AuthContext);
+  const token = authContext.authInfo.token;
+  const headers = token ? {authorization: token} : undefined;
+  const httpLink = new HttpLink({headers});
+  client.setLink(httpLink);
   return (
-    <ApolloProvider client={client}>
-      <View>
-        {/* <EquipmentScreen /> */}
-        <AuthScreen />
-      </View>
-    </ApolloProvider>
+    <AuthProvider>
+      <ApolloProvider client={client}>
+        <View>
+          {/* <EquipmentScreen /> */}
+          <AuthScreen />
+        </View>
+      </ApolloProvider>
+    </AuthProvider>
   );
 };
 
